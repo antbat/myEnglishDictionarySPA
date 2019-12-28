f77a4f05e5e3aeb3cfc7e87682c12261e4ee1cc4
Anton Batalin <antbat2009@gmail.com>
2019-12-28 11:05:49 +0200

    tag component

---
+++ src/app/public/components/tag/tag.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {BucketItems} from '../../models/SearchResult';
+import {Word} from '../../models/Word.model';
+import {WordService} from '../../service/word/word.service';
+
+@Component({
+  selector: 'app-tag',
+  templateUrl: './tag.component.html',
+  styleUrls: ['./tag.component.css']
+})
+export class TagComponent implements OnInit {
+    word: Word;
+    count: number;
+    @Input() set data(item: BucketItems) {
+        this.wordService
+            .getWordById(item.wordId)
+            .then( word => this.word = word );
+        this.count = item.count;
+    }
+    constructor(
+        private wordService: WordService
+    ) { }
+    ngOnInit() {
+    }
+
+}
