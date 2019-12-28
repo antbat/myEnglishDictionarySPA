e2d390b4d66bbd7b4f8a8013c654e6140c6f195e
Anton Batalin <antbat2009@gmail.com>
2019-12-28 11:06:41 +0200

    search page component

---
+++ src/app/public/search-page/search-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {SearchResult} from '../models/SearchResult';
+import {SearchService} from '../service/search/search.service';
+
+@Component({
+  selector: 'app-search-page',
+  templateUrl: './search-page.component.html',
+  styleUrls: ['./search-page.component.css']
+})
+export class SearchPageComponent implements OnInit {
+    results: SearchResult;
+    loading: boolean;
+
+    constructor(
+        private searchService: SearchService
+    ) { }
+    ngOnInit() {
+        this.searchService.word('*').subscribe( data => {
+            data.words = [];
+            this.results = data;
+        });
+    }
+    updateResults(results: SearchResult): void {
+        this.results = results;
+    }
+}
